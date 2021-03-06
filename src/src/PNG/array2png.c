610bdde9f8140edfb6a278f747a45f1f368068bb
Hans-Nikolai Viessmann <hv15@hw.ac.uk>
2021-03-06 18:47:05 +0100

    add writepng that supports file
    also move away from using tmpfile to mkstemp instead

--- src/src/PNG/array2png.c
+++ src/src/PNG/array2png.c
@@ -7,6 +7,113 @@
 #include <errno.h>
 #include <png.h>
 
+void SAC_PNG_array2png_file(SAC_ND_PARAM_out_nodesc( res_nt, int),
+                            SAC_ND_PARAM_in_nodesc( filep_nt, FILE*),
+                            SAC_ND_PARAM_in( ar_nt, int))
+{
+  png_structp png_ptr;
+  png_infop info_ptr;
+  png_bytep *row_pointers;
+  png_bytep img_data;
+  png_int_32 *bptr;
+  int x, y, aroffset, ioffset;
+  int height = SAC_ND_A_DESC_SHAPE(ar_nt, 0);
+  int width = SAC_ND_A_DESC_SHAPE(ar_nt, 1);
+  int *arr_data = SAC_ND_A_FIELD(ar_nt);
+  FILE *fp = SAC_ND_A_FIELD(filep_nt);
+
+  if (fp == NULL)
+  {
+    SAC_RuntimeError("SAC_PNG_array2png: Failed to open image file: %s",
+                     strerror(errno));
+    return;
+  }
+
+  fprintf (fp, "test test\n");
+  fflush (fp);
+
+  png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  if (png_ptr == NULL)
+  {
+    SAC_RuntimeError("SAC_PNG_array2png: Failed to create png structure: %s",
+                    strerror(errno));
+    return;
+  }
+
+  info_ptr = png_create_info_struct(png_ptr);
+  if (!info_ptr)
+  {
+    png_destroy_write_struct(&png_ptr, (png_infopp) NULL);
+    SAC_RuntimeError("SAC_PNG_array2png: Failed to create png info structure: %s",
+                    strerror(errno));
+    return;
+  }
+
+  if (setjmp(png_jmpbuf(png_ptr)))
+  {
+    png_destroy_write_struct(&png_ptr, &info_ptr);
+    SAC_RuntimeError("SAC_PNG_array2png: Failed to read PNG image");
+    return;
+  }
+
+  png_init_io(png_ptr, fp);
+  png_set_IHDR(png_ptr, info_ptr, width, height, 8,     /* bit depth */
+               PNG_COLOR_TYPE_RGB,      /* color type */
+               PNG_INTERLACE_NONE,      /* interlace type */
+               PNG_COMPRESSION_TYPE_DEFAULT,    /* compression type */
+               PNG_FILTER_TYPE_DEFAULT);        /* filter type */
+
+  png_write_info(png_ptr, info_ptr);
+
+  row_pointers = malloc(height * sizeof(png_bytep));
+  img_data = malloc(height * width * 4);
+  for (y = 0; y < height; y++)
+  {
+    row_pointers[y] = img_data + (y * width * 4);
+  }
+
+  /*
+   * draw
+   */
+  bptr = (png_int_32 *)img_data;
+  aroffset = 0;
+  ioffset = 0;
+  for (y = 0; y < height; y++) {
+    for (x = 0; x < width; x++) {
+      bptr[ioffset] = 
+	 ( (((png_int_32)SAC_ND_A_FIELD( ar_nt)[aroffset+0] & 0xff) <<  8) 
+	 | (((png_int_32)SAC_ND_A_FIELD( ar_nt)[aroffset+1] & 0xff) <<  16) 
+	 | (((png_int_32)SAC_ND_A_FIELD( ar_nt)[aroffset+2] & 0xff) <<  24) 
+         );
+      ioffset +=1;
+      aroffset+=3;
+    }
+  }
+  png_set_rows(png_ptr, info_ptr, row_pointers);
+
+  /*
+   * Write the image
+   */
+  png_set_filler(png_ptr, 0, PNG_FILLER_AFTER);
+  png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_FILLER, NULL);
+  png_write_end(png_ptr, info_ptr);
+
+  /*
+   * Clean up
+   */
+  free(row_pointers);
+  free(img_data);
+  png_destroy_write_struct(&png_ptr, &info_ptr);
+  fclose(fp);
+
+  /*
+   * return 0
+   */
+  *SAC_NAMEP( SAC_ND_A_FIELD( res_nt)) = 0;
+  SAC_ND_DEC_RC_FREE( ar_nt, 1,)
+
+}
+
 void SAC_PNG_array2png(SAC_ND_PARAM_out_nodesc( res_nt, int),
                        SAC_ND_PARAM_in_nodesc( string_nt, string),
                        SAC_ND_PARAM_in( ar_nt, int))
--- src/src/PNG/imagesac.h
+++ src/src/PNG/imagesac.h
@@ -8,5 +8,6 @@ typedef char* string;
 #define res_nt     (disp,  T_OLD((SCL, (HID, (NUQ,)))))
 #define ar_nt      (ar,    T_OLD((AKD, (NHD, (NUQ,)))))
 #define string_nt  (str,   T_OLD((SCL, (HID, (NUQ,)))))
+#define filep_nt   (filep, T_OLD((SCL, (HID, (NUQ,)))))
 
 #endif
